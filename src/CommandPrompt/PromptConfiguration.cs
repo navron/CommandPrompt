e16f81d58e37b95333baec85750a8703929f1e36
Navron <7018189+navron@users.noreply.github.com>
2019-09-19 14:55:00 +1000

    Put the parameter conversion back into its own file

--- src/CommandPrompt/PromptConfiguration.cs
+++ src/CommandPrompt/PromptConfiguration.cs
@@ -77,43 +77,9 @@ namespace CommandPrompt
 
         public object ParameterConvert(string text, Type parameterType)
         {
-            if (parameterType == typeof(string))
-            {
-                return text;
-            }
-            if (parameterType == typeof(int) && int.TryParse(text, out var resultInt))
-            {
-                return resultInt;
-            }
-            if (parameterType == typeof(ulong) && ulong.TryParse(text, out var resultULong))
-            {
-                return resultULong;
-            }
-            if (parameterType == typeof(int[]))
-            {
-                var listString = text.Split(new []{' '}, StringSplitOptions.None);
-                var listInt = new List<int>();
-                foreach (var s in listString)
-                {
-                    int.TryParse(s, out var resultIntA);
-                    listInt.Add(resultIntA);
-                }
-                return listInt.ToArray();
-            }
-            if (parameterType == typeof(string[]))
-            {
-                var listString = text.Split(new[] { ' ' }, StringSplitOptions.None);
-                var listInt = new List<int>();
-                foreach (var s in listString)
-                {
-                    int.TryParse(s, out var resultIntA);
-                    listInt.Add(resultIntA);
-                }
-                return listInt.ToArray();
-            }
-
-            // Parameter Type not coded for, either subclass this Configuration class or ask for type to be handled
-            return null;
+            // Valid to return null as Parameter Type not coded for,
+            // either subclass this Configuration class or ask for type to be handled
+            return ParameterConversion.Convert(text, parameterType);
         }
 
         #endregion
